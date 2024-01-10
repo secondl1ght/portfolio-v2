8c0aee6ab45899061fade4b9741327481ec4e1ea
secondl1ght <secondl1ght@protonmail.com>
2024-01-09 23:55:10 -0700

    fix import

--- src/lib/comp.ts
+++ src/lib/comp.ts
@@ -12,6 +12,5 @@ export { default as Footer } from '../components/Footer.svelte';
 export { default as Header } from '../components/Header.svelte';
 export { default as Hero } from '../components/Hero.svelte';
 export { default as ProjectCard } from '../components/ProjectCard.svelte';
-export { default as ProjectMiniCard } from '../components/ProjectMiniCard.svelte';
 export { default as Support } from '../components/Support.svelte';
 export { default as Tech } from '../components/Tech.svelte';
